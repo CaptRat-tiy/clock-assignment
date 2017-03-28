0cd9136f287078b923a62051786c5e190a3cfc6a
Patrick Logan <JPatrickLogan@gmail.com>
2017-03-27 21:30:51 -0600

    feat (.js) Add time logic to .js

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -1,3 +1,37 @@
+// Datepicker script
+
 $( function() {
   $( "#datepicker" ).datepicker();
 } );
+
+function updateTimer(deadline){
+  var time = deadline - new Date();
+  return {
+    'days': Math.floor( time/(1000*60*60*24) ),
+    'hours': Math.floor( time/(1000*60*60) % 24 ),
+    'minutes': Math.floor( (time/1000*60) % 60 ),
+    'seconds': Math.floor( (time/1000)% 60 ),
+    'total' : time
+  };
+}
+
+function startTimer(id, deadline){
+  var timerInterval = setInterval(function(){
+    var clock = document.getElementById(id);
+    var timer = updateTimer(deadline);
+
+    clock.innerHTML = '<span>' + timer.days + '</span>'
+                    + '<span>' + timer.hours + '</span>'
+                    + '<span>' + timer.minutes + '</span>'
+                    + '<span>' + timer.seconds + '</span>'
+
+
+
+
+  }, 1000);
+}
+
+window.onload = function(){
+  var deadline= new Date("April 25, 2018 17:15:00");
+  startTimer("clock", deadline);
+};
