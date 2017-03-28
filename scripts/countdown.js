92990eed0d831c6a4303ba238df8ace998f2a36e
Patrick Logan <JPatrickLogan@gmail.com>
2017-03-27 22:07:49 -0600

    feat (css) Add clock tiles and add jQuery Date-picker;

--- scripts/countdown.js
+++ scripts/countdown.js
@@ -8,9 +8,9 @@ function updateTimer(deadline){
   var time = deadline - new Date();
   return {
     'days': Math.floor( time/(1000*60*60*24) ),
-    'hours': Math.floor( time/(1000*60*60) % 24 ),
-    'minutes': Math.floor( (time/1000*60) % 60 ),
-    'seconds': Math.floor( (time/1000)% 60 ),
+    'hours': Math.floor( (time/(1000*60*60)) % 24 ),
+    'minutes': Math.floor( (time/1000/60) % 60 ),
+    'seconds': Math.floor( (time/1000) % 60 ),
     'total' : time
   };
 }
@@ -25,8 +25,16 @@ function startTimer(id, deadline){
                     + '<span>' + timer.minutes + '</span>'
                     + '<span>' + timer.seconds + '</span>'
 
+    var spans = clock.getElementsByTagName("span");
+    animateClock(spans[3]);
+    animateClock(spans[2]);
+    animateClock(spans[1]);
+    animateClock(spans[0]);
 
-
+                    if(timer.total < 1){
+                      clearInterval(timerInterval);
+                      clock.innerHTML = "Time's Up!"
+                    }
 
   }, 1000);
 }
